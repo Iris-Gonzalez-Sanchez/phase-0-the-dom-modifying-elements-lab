004010d50d3da8c960ecd63496ede00bd9ee6520
Iris-Gonzalez-Sanchez <Iris.GonzalezSan@Gmail.com>
2022-05-06 19:28:00 -0400

    commit tree ca6f96237e7788d750d44b6690b310fac8207f71

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code here!
+document.querySelector('main#main').remove();
+//main.remove();
+
+const newHeader = document.createElement("h1");
+//document.body.append(newHeader);
+//newHeader.h1 = 'h1#victory';
+
+newHeader.id = "victory";
+
+newHeader.innerHTML = "Iris is the champion";
+
+/*const main = document.getElementById("main");
+newHeader.innerHTML = "YOUR-NAME is the champion";*/
+
